3bff42c8f9d06e4beef5e6f7ee7970e33de0d455
Letícia Dupont <leticia.dupont20@gmail.com>
2022-07-08 13:18:43 -0300

    eureka

--- hr-payroll/src/main/java/com/shdupont/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/shdupont/hrpayroll/services/PaymentService.java
@@ -3,27 +3,18 @@ package com.shdupont.hrpayroll.services;
 import com.shdupont.hrpayroll.entities.Payment;
 
 import com.shdupont.hrpayroll.entities.Worker;
+import com.shdupont.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    @Autowired
-    private RestTemplate restTemplate;
+   @Autowired
+   private WorkerFeignClient workerFeignClient;
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariable = new HashMap<>();
-        uriVariable.put("id", ""+workerId);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
--- hr-worker/src/main/java/com/dupont/microsservice/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/dupont/microsservice/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.dupont.microsservice.resources;
 
 import com.dupont.microsservice.entities.Worker;
 import com.dupont.microsservice.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,10 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
     @Autowired
     private WorkerRepository workerRepository;
 
@@ -26,6 +33,8 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("Port = " + env.getProperty("local.server.port"));
+
         Worker obj = workerRepository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
