c1a6ac94d78bbe4e90aabd47a9ac9e9c263e24c6
Letícia Dupont <leticia.dupont20@gmail.com>
2022-07-08 11:37:11 -0300

    hr-payroll implementation

--- hr-payroll/src/main/java/com/shdupont/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/shdupont/hrpayroll/services/PaymentService.java
@@ -1,11 +1,30 @@
 package com.shdupont.hrpayroll.services;
 
 import com.shdupont.hrpayroll.entities.Payment;
+
+import com.shdupont.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
+
 }
