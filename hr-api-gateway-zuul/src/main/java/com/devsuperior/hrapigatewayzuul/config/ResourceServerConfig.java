4abac380b6d69e3594eb3bb4aad3443919040e20
Letícia Dupont <leticia.dupont20@gmail.com>
2022-07-11 18:17:57 -0300

    dev profile

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -30,7 +30,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String[] OPERATOR = { "/hr-worker/**" };
 
-    private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/hr-user/actuator/**" };
+    private static final String[] ADMIN = { "/hr-payroll/**", "/actuator/**", "/hr-worker/actuator/**", "/hr-oauth/actuator/**" };
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
